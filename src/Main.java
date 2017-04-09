99d0fd6012a9eb239559c2c108ec0e280cf0201b
stefan2212 <stefan.cernescu94@gmail.com>
2017-04-09 17:55:44 +0100

    Implemented add, isCircular, removeLink by value

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,13 @@
 
-class Link{
+class Link {
     public int value;
     public Link next;
 
-    public Link(int value){
-        this.value=value;
+    public Link(int value) {
+        this.value = value;
     }
 
-    public int getValue(){
+    public int getValue() {
         return value;
     }
 
@@ -18,47 +18,96 @@ public class Main {
 
     public static void main(String[] args) {
         LinkedList lista = new LinkedList();
-        lista.insert(2);
+        Link node = new Link(2);
+        lista.insert(node);
         lista.insert(3);
         lista.insert(4);
         lista.insert(5);
+        lista.removeByValue(4);
         lista.displayList();
     }
 }
 
-class LinkedList{
+class LinkedList {
     public Link firstLink;
-    LinkedList(){
-        this.firstLink=null;
+
+    LinkedList() {
+        this.firstLink = null;
     }
 
-    public boolean isVid(){
-        return firstLink==null;
+    public boolean isVid() {
+        return firstLink == null;
     }
 
-    public void insert(int value){
+    public void insert(int value) {
         Link newLink = new Link(value);
-        if (isVid()==true)
-        {
-            firstLink=newLink;
-        }
-        else
-        {
-            Link currentLink=firstLink;
-            while(currentLink.next!=null){
-                currentLink=currentLink.next;
+        if (isVid() == true) {
+            firstLink = newLink;
+        } else {
+            Link currentLink = firstLink;
+            while (currentLink.next != null) {
+                currentLink = currentLink.next;
             }
-            currentLink.next=newLink;
+            currentLink.next = newLink;
         }
-
     }
 
-    public void displayList(){
-        Link theLink = firstLink;
-        while (theLink!=null)
-        {
-            System.out.println(theLink.getValue());
-            theLink=theLink.next;
+    public void insert(Link node) {
+        if (isVid() == true)
+            firstLink = node;
+        else {
+            Link currentLink = firstLink;
+            while (currentLink.next != null) {
+                currentLink = currentLink.next;
+            }
+            currentLink.next = node;
         }
+    }
+
+    public void displayList() {
+        Link theLink = firstLink;
+        while (theLink != null) {
+            System.out.println(theLink.getValue());
+            theLink = theLink.next;
+            if (theLink == firstLink) {
+                System.out.print(theLink.getValue());
+                return;
+            }
+        }
+    }
+
+    public boolean isCircular() {
+        Link theLink = firstLink;
+        while (theLink != null) {
+            theLink = theLink.next;
+            if (theLink == firstLink)
+                return true;
+        }
+        return false;
+    }
+
+    public Link remoFirstLink() {
+        Link linkReference = firstLink;
+        while (firstLink.next != null) {
+            firstLink = firstLink.next;
+        }
+        return linkReference;
+    }
+
+    public Link removeByValue(int value) {
+        Link currentLink = firstLink;
+        Link previousLink = firstLink;
+
+        while (currentLink.getValue() != value && currentLink != null) {
+            previousLink = currentLink;
+            currentLink = currentLink.next;
+        }
+        if (currentLink==firstLink) {
+            firstLink = firstLink.next;
+        }
+        else{
+            previousLink.next=currentLink.next;
+        }
+        return currentLink;
     }
 }
